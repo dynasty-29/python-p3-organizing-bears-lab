a28f54ccb87deb974ffe7ccc2a7825368011368c
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-02-18 13:32:42 +0300

    queries updated

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr.Chocolate", 3, "M","brown","fiesty", "1")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 2, "F","white", "calm", "1")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 3, "F","black","playful", "0")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergent Brown", 4, "M","cream","scrachy", "1")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 5, "F","white","proud", "0")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 1, "M","green","fiesty", "1")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 3, "F","brown","lazy", "1")
-INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 1, "M","black","cunning", "1")
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr.Chocolate", 3, "M","brown","fiesty", "1"),
+("Rowdy", 2, "F","white", "calm", "1"),
+("Tabitha", 3, "F","black","playful", "0"),
+("Sergent Brown", 4, "M","cream","scrachy", "1"),
+("Melissa", 5, "F","white","proud", "0"),
+("Grinch", 1, "M","green","fiesty", "1"),
+("Wendy", 3, "F","brown","lazy", "1"),
+(NULL, 1, "M","black","cunning", "1")
